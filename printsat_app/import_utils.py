f14ed925e8246358d30b9e50d5d37cc4b3e75f18
cyface <tim@cyface.com>
2014-07-31 08:16:04 -0600

    Basic working import from file and extract download.

---
+++ printsat_app/import_utils.py
@@ -0,0 +1,66 @@
+"""
+##############################################
+#####-----Printsat Data Importer-------#######
+##############################################
+"""
+
+# pylint: disable=E0611,F0401,W0401,W0614,W0612,R0914,R0902
+
+import csv  # first we need import necessary lib:csv
+import datetime
+import os
+from printsat_app.models import *
+from django.conf import settings
+from django.utils.timezone import utc
+
+
+def import_data():
+    """Import Data"""
+
+    telemetry_header_file = open(os.path.join(settings.BASE_DIR, "printsat_app", "formats", "printsat_telemetry_headers.csv"))
+    telemetry_headers = csv.DictReader(telemetry_header_file)
+    telemetry_file = open(os.path.join(settings.BASE_DIR, "printsat_app", "test_data", "WD0E_DumpCalc_10292012.csv"))
+    telemetry_list = csv.DictReader(telemetry_file, fieldnames=telemetry_headers.fieldnames)
+
+    # Loop through the rows in the CSV
+    for row in telemetry_list:
+        # Create a telemetry object in the database based on the current row
+
+        if telemetry_list.line_num == 1:
+            station = row.get('ps_time')
+        elif telemetry_list.line_num == 2:
+            lat = row.get('ps_time')
+            lon = row.get('ps_time_calc')
+        elif telemetry_list.line_num == 3:
+            program = row.get('ps_time')
+            telem_type = row.get('ps_time_calc')
+        elif telemetry_list.line_num == 4:
+            pass  # Header Row
+        else:
+            row.pop("unused1", None)
+            row.pop("unused2", None)
+            row.pop("unused3", None)
+            row.pop("unused4", None)
+            row.pop("unused5", None)
+            row.pop("unused6", None)
+            row.pop("unused7", None)
+            row.pop("unused8", None)
+            row['station'] = station
+            row['lat'] = lat
+            row['lon'] = lon
+            row['program'] = program
+            row['telem_type'] = telem_type
+            time_value = time.strptime(row.get('ps_time', '01.01.1900 00:00:00'), '%m.%d.%Y %H:%M:%S')
+            row['ps_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time_value) + "+0000"
+            row['ps_time_calc'] = datetime.datetime.fromtimestamp(float(row.get('ps_time_calc')), tz=utc)
+
+            telemetry = Telemetry.objects.create(**row)
+
+            # Save the telemetry!
+            telemetry.save()
+            print telemetry
+
+
+# #### MAIN FUNCTION TO RUN IF THIS SCRIPT IS CALLED ALONE ##
+if __name__ == "__main__":
+    import_data()
--- printsat_app/views.py
+++ printsat_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import csv
+from django.views.generic import View, TemplateView
+from django.http import HttpResponse
+from printsat_app.models import Telemetry
+from django.utils.encoding import smart_str
 
-# Create your views here.
+
+class HomePage (TemplateView):
+    template_name = 'home.html'
+
+
+class CannedExtract (View):
+
+    def get(self, request, *args, **kwargs):
+        queryset = Telemetry.objects.all()[:5]
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=telemetry.csv'
+        writer = csv.writer(response, csv.excel)
+        response.write(u'\ufeff'.encode('utf8'))
+        writer.writerow([
+            smart_str(u"ID"),
+            smart_str(u"PS_Time"),
+            smart_str(u"Station"),
+        ])
+        for obj in queryset:
+            writer.writerow([
+                smart_str(obj.pk),
+                smart_str(obj.ps_time),
+                smart_str(obj.station),
+            ])
+        return response
