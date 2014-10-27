ff935f7481fb6945f5c4bad4b37f254265deb9a6
Tim White <tim@cyface.com>
2014-10-27 10:12:41 -0600

    removing admin.autodiscover with upgrade to Django 1.7

--- printsat/urls.py
+++ printsat/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from printsat_app.views import HomePage, ExtractCSV, ExtractPage, UploadPage
 
-admin.autodiscover()
-
 urlpatterns = patterns('',
                        url(r'^$', HomePage.as_view(), name="home"),
                        url(r'^extract/csv/(?P<format_name>\w+)', ExtractCSV.as_view(), name="extract_csv"),
